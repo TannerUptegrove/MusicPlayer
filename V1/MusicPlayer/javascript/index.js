eaf3809d4415599ba98d40873755125275e48ce7
Tanner Uptegrove <tanneruptegrove@gmail.com>
2025-07-14 13:07:31 -0600

    Dim Background Implementation Fixed the load playlist button to now dim the background.

--- V1/MusicPlayer/javascript/index.js
+++ V1/MusicPlayer/javascript/index.js
@@ -3,9 +3,19 @@ let appTitle = document.getElementById("app-title");
 let instructionsText = document.getElementById("instructions-text");
 let trackLoaderBtn = document.getElementById("track-loader-btn");
 
+let body = document.querySelector("body");
+let backgroundDesign = document.querySelector(".background-design");
+
 trackLoaderBtn.addEventListener("click", function(event) {
     trackLoaderPanel.style.gap = "200px";
     trackLoaderPanel.classList.add("track-loader-panel-animation");
     instructionsText.style.display = "none";
     trackLoaderBtn.innerHTML = "Load Again";
+
+    body.classList.add("dim-background-animation");
+    backgroundDesign.classList.add("dim-background-animation");
 });
+
+function LoadPlaylists() {
+    
+}
